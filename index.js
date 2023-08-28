4c858c092205acfc094820c1ed4b3b2633baac3e
Jonathan <jon8921@hotmail.com>
2023-08-28 12:10:00 -0500

    d

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
 }
+function() {
+  alert('I was clicked!');
+}
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
