ed00f55c7860c3efb81305b335f3b47ab3f1dda7
Jonathan <jon8921@hotmail.com>
2023-08-28 12:34:09 -0500

    d

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 function addingEventListener() {
-}
-function() {
+  const input = document.getElementById('button');
+input.addEventListener('click', function() {
   alert('I was clicked!');
+});
 }
-const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert);
